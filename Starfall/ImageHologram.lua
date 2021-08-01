d7e400a830e2cf75960d37d3088c402e7e192dc4
ravi0li <mikel.veg777@gmail.com>
2021-08-01 20:31:06 +0200

    v1.3

--- Starfall/ImageHologram.lua
+++ Starfall/ImageHologram.lua
@@ -1,7 +1,9 @@
+--@name Anime girl probably
 --@client
 
 local url = "https://u.teknik.io/u7KBU.png"
 local myMaterial = material.create("VertexLitGeneric")
+local holoSize = 100
 
 local function loadCallback( mat, url, w, h, layout )
   if not url then return end
@@ -13,9 +15,11 @@ myMaterial:setTextureURL( "$basetexture", url, loadCallback, function() end )
 
 myMaterial:setInt("$flags", 0)
 
-local holo = holograms.create( chip():getPos() + Vector( 0, -10, 65 ), Angle(90,-90,0), "models/holograms/plane.mdl" )
+local holo = holograms.create( chip():getPos() + Vector( 0, -5, holoSize/1.9 ), Angle(90,-90,0), "models/holograms/plane.mdl" )
 
-holo:setSize(Vector(300, 300, 100))
+holo:setSize(Vector(holoSize, holoSize, holoSize))
 holo:setParent(chip())
 
 holo:setMaterial("!" .. myMaterial:getName())
+
+print("Done. Check the Y coordinate.")  -- I kept forgoting the holo is created facing Y
